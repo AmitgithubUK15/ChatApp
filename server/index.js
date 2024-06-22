47bfaccbeb2f3bb133719ba784ccb949e5562c32
Amit <Amitshah050112003@gmail.com>
2024-06-23 00:59:11 +0530

    connect to database (MongoDB)

--- server/index.js
+++ server/index.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const dotenv = require("dotenv");
-
+const mongoose = require("mongoose");
 
 dotenv.config();
 const port = process.env.PORT || 3000;
 const app = express();
 
+mongoose.connect(process.env.URI)
+.then(()=>{
+    console.log("Connected to MongoDB");
+})
+.catch((error)=>{
+    console.log(error);
+})
 
 app.get("/",(req,res)=>{
     console.log("hello");
